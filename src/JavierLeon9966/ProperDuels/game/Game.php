f95d1e330fd2d3d927099c8003af1c9aeee9f48d
Covered123 <58715544+JavierLeon9966@users.noreply.github.com>
2022-06-05 00:22:26 -0300

    Do not close the current window twice

--- src/JavierLeon9966/ProperDuels/game/Game.php
+++ src/JavierLeon9966/ProperDuels/game/Game.php
@@ -84,7 +84,6 @@ final class Game{
 			$session->setGame($this);
 
 			$player = $session->getPlayer();
-			$player->removeCurrentWindow();
 			$session->saveInfo();
 
 			$properDuels->getQueueManager()->remove($player->getUniqueId()->getBytes());
